c096738c02efd046828f49ee64cacc86de9d50b7
Keith Wygant <keith.wygant@gmail.com>
2017-04-11 01:23:20 -0400

    added a couple of symbol-constructing procedures.

--- fbp.el
+++ fbp.el
@@ -1,7 +1,5 @@
 (require 'dash)
 
-(defalias fbp-custom-var #'customize-set-variable)
-
 (defun fbp-custom-vars (&rest ASSOCS)
   "For each (SYMBOL . VALUE) of ASSOCS, customize SYMBOL to VALUE with `customize-set-variable'.
 Example:
@@ -10,29 +8,76 @@ Example:
   '(bar . (A B C)) ; hopefully more clear than '(bar A B C)
  )"
   (dolist (pair ASSOCS)
-    (fbp-custom-var (car pair) (cdr pair))))
+    (customize-set-variable (car pair) (cdr pair))))
 
 (defun fbp-def-face (GROUP SYMBOL DOCSTRING &rest PROPERTIES)
-  "Create a face called SYMBOL with DOCSTRING and PROPERTIES in customization group GROUP.
-The display is always t. If you are worried about unsupported properties, use `display-supports-face-attributes-p' to filter PROPERTIES.
-Example:
- (fbp-def-face 'my-lame-stuff
-               'my-lame-face
-               \"a lame face\"
-               :weight 'ultra-light
-               :box t)"
   (custom-declare-face SYMBOL
                        (list (cons t PROPERTIES))
                        DOCSTRING
                        :group GROUP))
 
 (defun fbp-def-faces (GROUP &rest FACES)
-  "For each face of the form (SYMBOL DOCSTRING . PROPERTIES) in FACES, use `fbp-def-face' to create a face called SYMBOL with DOCSTRING and PROPERTIES in customization group GROUP.
+  "For each face of the form (SYMBOL DOCSTRING . PROPERTIES) in FACES, create a face called SYMBOL with DOCSTRING and PROPERTIES in customization group GROUP.
 Example:
  (fbp-def-faces 'my-lame-stuff
     '(my-lame-face \"a lame face\" :weight ultra-light :box t)
     '(my-reverse-face \"a reversed face\" :slant reverse-oblique :inverse-video t :inherit my-lame-face)
  )"
   (declare (indent 2))
+  (mapcar (lambda (face)
+            (apply #'fbp-def-face GROUP face))
+          FACES))
+
+(defun fbp-def-faces2 (FACES &optional GROUP)
+  "For each face of the form (SYMBOL DOCSTRING . PROPERTIES) in FACES, create a face called SYMBOL with DOCSTRING and PROPERTIES in customization group GROUP.
+Example:
+ (fbp-def-faces '(
+    (my-lame-face \"a lame face\" :weight ultra-light :box t)
+    (my-reverse-face \"a reversed face\" :slant reverse-oblique :inverse-video t :inherit my-lame-face)
+    ) 'my-lame-stuff)"
   (dolist (face FACES)
-    (apply #'fbp-def-face GROUP face)))
+    (-let [(symbol docstring . properties) face]
+      (custom-declare-face symbol
+                           (list (cons t properties))
+                           docstring
+                           :group GROUP))))
+
+(defun fbp-make-face-adaptive (GROUP TEST ACTIVE-DOC.PROPS INACTIVE-DOC.PROPS &optional NAME)
+  "Create GROUP-NAME-active-face and GROUP-NAME-inactive-face, and a function that returns one of them based on TEST."
+  (-let* (
+          (prefix (concat (symbol-name GROUP)
+                          "-"
+                          (when NAME (concat NAME "-"))))
+          ((active-face inactive-face)
+           (fbp-def-faces
+            GROUP
+            (cons (intern (concat prefix "active-face"))
+                  ACTIVE-DOC.PROPS)
+            (cons (intern (concat prefix "inactive-face"))
+                  INACTIVE-DOC.PROPS)))
+          )
+    (fset (intern (concat prefix "face"))
+          `(lambda ()
+             (if (funcall ,TEST)
+                 ,active-face
+               ,inactive-face)))))
+
+(defun fbp-make-hook (WHEN PROCEDURE &optional CONTINGENT)
+  "Create hook WHEN-PROCEDURE-hook to run WHEN PROCEDURE is called, unless it is already defined. The CONTINGENT functions are added to the hook regardless."
+  (let* ((when-str (substring (symbol-name WHEN) 1))
+         (proc-name (symbol-name PROCEDURE))
+         (hook-name (concat when-str "-" proc-name "-hook"))
+         (existing-hook (intern-soft hook-name))
+         (hook-symbol (or existing-hook (intern hook-name))))
+    (unless existing-hook
+      (set hook-symbol nil)
+      (put hook-symbol 'variable-documentation
+           (concat "procedures to run " when-str " `" proc-name "'"))
+      (advice-add
+       PROCEDURE
+       WHEN
+       (lambda (&rest _)
+         (run-hooks `,hook-symbol))))
+    (dolist (contingent-proc (reverse CONTINGENT))
+      (add-hook hook-symbol contingent-proc))
+    hook-symbol))
